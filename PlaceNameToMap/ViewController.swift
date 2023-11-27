b14f335ccdb7dda581efb57f21fed2d3649d9b4c
Kuang-Yu <kuangyu.nien@somnics.com>
2023-11-27 10:00:37 +0800

    Initial Commit

---
+++ PlaceNameToMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PlaceNameToMap
+//
+//  Created by SomnicsAndrew on 2023/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
